464a622c5f1cca0046d2227cb5ac7ef490c22ecb
Andrew Rayskiy <andy@spdg.net>
2019-10-08 01:53:35 +0300

    Add parsers for 'pb' and 'probdef'.

---
+++ problemparser.py
@@ -0,0 +1,92 @@
+import re, os
+from enum import Enum
+
+
+class ProblemType(Enum):
+    # Supported problem types.
+    pb = 1,
+    probdef = 2
+
+
+class ProblemParser:
+    def __init__(self, problem_str):
+        self.type = problem_type
+        self._id, self._name, self._link = ["" for _ in range(3)]
+        self._tl, self._ml = [0 for _ in range(2)]
+        self.parse(problem_str)
+
+    def parse(self, problem_str):
+        raise NotImplementedError
+
+    @staticmethod
+    def normalize_tl(tl):
+        if len(tl) <= 2:
+            return int(tl)
+        if int(tl) % 1000 == 0:
+            return int(tl) // 1000
+        else:
+            return float(tl) / 1000
+
+    @staticmethod
+    def normalize_ml(ml):
+        ml = int(ml)
+        if ml > 1024:
+            return ml // 1024
+        return ml
+
+    def id(self):
+        return self._id
+
+    def name(self):
+        return self._name
+
+    def link(self):
+        return self._link
+
+    def tl(self):
+        return self._tl
+
+    def ml(self):
+        return self._ml
+
+
+class ProblemParserPb(ProblemParser):
+    def __init__(self, problem_str):
+        super().__init__(problem_str)
+
+    def parse(self, problem_str):
+        # TODO: fix parameter parsing to support " and , inside declarations.
+        params = [x.strip().strip('"') for x in re.findall('\((.*?)\)', problem_str)[0].split(',')]
+        self._id, self._name = params[:2]
+        # This particular problem type targets everything under burunduk1/problems/yyyy-mm/<problem>.
+        self._link = os.path.join('burunduk1', 'problems', params[3], params[2])
+        self._tl = ProblemParser.normalize_tl(params[-2])
+        self._ml = ProblemParser.normalize_ml(params[-1])
+
+
+class ProblemParserProbdef(ProblemParser):
+    def __init__(self, problem_str):
+        super().__init__(problem_str)
+
+    def parse(self, problem_str):
+        # TODO: fix parameter parsing to support " and , inside declarations.
+        params = [x.strip().strip('"') for x in re.findall('\((.*?)\)', problem_str)[0].split(',')]
+        # Trim " where applicable.
+        self._id, self._name, self._link = [x[1:-1] for x in params[:3]]
+        self._tl = ProblemParser.normalize_tl(params[-2])
+        self._ml = ProblemParser.normalize_ml(params[-1])
+
+
+def problem_type(problem_str):
+    for problem_type in ProblemType:
+        regex = '{}\d{{0,1}} *\("'.format(problem_type.name)
+        if re.match(regex, problem_str):
+            return problem_type
+    return None
+
+
+def create(problem_str, problem_type):
+    if problem_type == ProblemType.pb:
+        return ProblemParserPb(problem_str)
+    elif problem_type == ProblemType.probdef:
+        return ProblemParserProbdef(problem_str)
